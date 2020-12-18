4666391aa514ba2724e87540a3a675b70d75ae18
Leonardo Marcelino <leonardo.marcelino@gmail.com>
2020-12-18 10:39:42 -0300

    Add notification when each upload is finished

--- src/zip-stream-download-extract-upload.ts
+++ src/zip-stream-download-extract-upload.ts
@@ -3,33 +3,43 @@ import path from 'path';
 import stream from 'stream';
 import AWS from 'aws-sdk';
 
-const BUCKET = "pf-test-zip-bucket"
-const extensions = [".xlsx", ".pdf"]
-const s3: AWS.S3 = new AWS.S3({ apiVersion: '2006-03-01' });
-const params: any = { Bucket: BUCKET, Key: 'zip/test.zip' };
+const BUCKET = 'pf-test-zip-bucket';
+const extensions = ['.xlsx', '.pdf'];
+const s3: AWS.S3 = new AWS.S3({apiVersion: '2006-03-01'});
+const params: any = {Bucket: BUCKET, Key: 'zip/test.zip'};
+
+let totalFiles = 0;
+let uploadedFiles = 0;
 
 s3.getObject(params).createReadStream()
-  .pipe(unzip.Parse())
-  .on('entry', (entry: any) => {
-    if (extensions.includes(path.extname(entry.path))
-      && !path.basename(entry.path).startsWith(".")) {
-      console.log('Uploading file', path.basename(entry.path));
+    .pipe(unzip.Parse())
+    .on('entry', (entry: any) => {
+      if (extensions.includes(path.extname(entry.path)) &&
+      !path.basename(entry.path).startsWith('.')) {
+        const {writeStream, promise} = uploadStream({
+          Bucket: BUCKET,
+          Key: `unzipped/${path.basename(entry.path)}`,
+        });
 
-      const { writeStream } = uploadStream({
-        Bucket: BUCKET,
-        Key: `unzipped/${path.basename(entry.path)}`
-      });
+        totalFiles++;
+        entry.pipe(writeStream);
 
-      entry.pipe(writeStream)
-    } else {
-      entry.autodrain();
-    }
-  })
+        promise.then((data: any) => {
+          uploadedFiles++;
+          console.log('Upload finished', data);
+          if (totalFiles == uploadedFiles) {
+            console.log('All Done!!!');
+          }
+        });
+      } else {
+        entry.autodrain();
+      }
+    });
 
-const uploadStream = ({ Bucket, Key }) => {
+const uploadStream = ({Bucket, Key}) => {
   const pass = new stream.PassThrough();
   return {
     writeStream: pass,
-    promise: s3.upload({ Bucket, Key, Body: pass }).promise(),
+    promise: s3.upload({Bucket, Key, Body: pass}).promise(),
   };
-}
+};
